9a9cdc976f1958e8a5cab96926a9484451529454
Filip Glura <lockontargt@gmail.com>
2021-08-14 20:05:59 +0200

    feat: next conversion context consumer to useContext hook

--- components/organisms/WeatherMoreInfo/WeatherInfoItem/WeatherInfoItem.js
+++ components/organisms/WeatherMoreInfo/WeatherInfoItem/WeatherInfoItem.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Image from "next/image";
 import styles from './WeatherInfoItem.module.scss';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import weatherContext from "context";
 
-const WeatherInfoItem = ({ imageSource, type, unit }) => (
-    <div className={styles.wrapper}>
-        <weatherContext.Consumer>
-            {(weather) => 
-                <>
-                    <Image alt="weather" width={38} height={38} src={imageSource} />
-                    { unit ? <Paragraph>{`${weather.current[type]} ${unit}`}</Paragraph> : <Paragraph>{weather.current[type]}</Paragraph>}
-                    
-                </>
-            }
-        </weatherContext.Consumer>
-    </div>
-);
+const WeatherInfoItem = ({ imageSource, type, unit }) => {
+    const weather = useContext(weatherContext);
+    return (
+        <div className={styles.wrapper}>
+            <Image alt="weather" width={38} height={38} src={imageSource} />
+            { unit ? <Paragraph>{`${weather.current[type]} ${unit}`}</Paragraph> : <Paragraph>{weather.current[type]}</Paragraph>}
+        </div>
+    );
+};
 
 export default WeatherInfoItem;

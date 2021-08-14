f6ec41ee2a2f3641c052fe476b5dfe5d66989bf8
Filip Glura <lockontargt@gmail.com>
2021-08-14 19:51:27 +0200

    feat: conversion context consumer to useContext hook

--- components/molecules/SingleHourWeather/SingleHourWeather.js
+++ components/molecules/SingleHourWeather/SingleHourWeather.js
@@ -1,32 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import Image from "next/image";
 import styles from "./SingleHourWeather.module.scss";
 import Paragraph from "components/atoms/Paragraph/Paragraph";
 import weatherContext from "context";
 
-const SingleHourWeather = ({ day, hour }) => (
-    <weatherContext.Consumer>
-        {(weather) => (
-            <div className={styles.SingleHourWeather}>
-                <Paragraph>{hour}:00</Paragraph>
-                <div className={styles.details}>
-                    <Image
-                        src={`https:${weather.forecast.forecastday[day].hour[hour].condition.icon}`}
-                        alt={`${hour} forecast weather icon`}
-                        width={64}
-                        height={64}
-                    />
-                    <div className={styles.detailsText}>
-                        <Paragraph>{weather.forecast.forecastday[day].hour[hour].condition.text}</Paragraph>
-                        <Paragraph>{weather.forecast.forecastday[day].hour[hour].wind_kph} kph</Paragraph>
-                    </div>
-                    <Paragraph isBig>
-                        {weather.forecast.forecastday[day].hour[hour].temp_c}°C
-                    </Paragraph>
+const SingleHourWeather = ({ day, hour }) => {
+    const weather = useContext(weatherContext);
+    return (
+        <div className={styles.SingleHourWeather}>
+            <Paragraph>{hour}:00</Paragraph>
+            <div className={styles.details}>
+                <Image
+                    src={`https:${weather.forecast.forecastday[day].hour[hour].condition.icon}`}
+                    alt={`${hour} forecast weather icon`}
+                    width={64}
+                    height={64}
+                />
+                <div className={styles.detailsText}>
+                    <Paragraph>{weather.forecast.forecastday[day].hour[hour].condition.text}</Paragraph>
+                    <Paragraph>{weather.forecast.forecastday[day].hour[hour].wind_kph} kph</Paragraph>
                 </div>
+                <Paragraph isBig>
+                    {weather.forecast.forecastday[day].hour[hour].temp_c}°C
+                </Paragraph>
             </div>
-        )}
-    </weatherContext.Consumer>
-);
+        </div>
+    );
+};
 
 export default SingleHourWeather;
